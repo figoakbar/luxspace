307e1ee4e48571243ffe3dcb286ca406e1deb201
Figo Akbar <figoakbar15@gmail.com>
2023-06-06 14:15:37 +0700

    Use scroll anchor and Use Modal DOM at homepage

--- luxspace/src/pages/HomePage.jsx
+++ luxspace/src/pages/HomePage.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from '../parts/Header'
-import Hero from '../parts/Hero'
+import Hero from '../parts/Homepage/Hero'
 import JustArrived from '../parts/Homepage/JustArrived'
 import BrowseRoom from '../parts/Homepage/BrowseRoom'
 import Clients from '../parts/Homepage/Clients'
 import Sitemap from '../parts/Homepage/Sitemap'
 import Footer from '../parts/Footer'
 
+import useScrollAnchor from '../helpers/hooks/useScrollAnchor'
+import useModalDOM from '../helpers/hooks/useModalDOM'
+
 export default function HomePage() {
+  useScrollAnchor();
+  useModalDOM();
   return (
     <>
       <Header theme="white" position="absolute"/>
